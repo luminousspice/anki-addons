034ca98c59d3b36faf591ddc314f805955fc370e
Luminous Spice <luminous.spice@gmail.com>
2016-05-09 15:57:21 +0900

    Feed to Anki: Change urllib to urllib2

--- myrss.py
+++ myrss.py
@@ -1,52 +1,64 @@
 # -*- coding: utf-8 -*-
 
-from aqt import mw
 import BeautifulSoup
-import urllib
+import urllib2
+from aqt import mw
 from aqt.qt import *
 
 URL = "http://www.merriam-webster.com/wotd/feed/rss2"
-tags = ["MW"]
 DECK = "Words"
 MODEL = "words"
+tags = ["MW"]
 
 def buildCard():
-    # MAKE DECK
+    # get deck and model
     deck  = mw.col.decks.get(mw.col.decks.id(DECK))
     model = mw.col.models.byName(MODEL)
 
-    # ASSIGN MODEL TO DECK
+    # assign model to deck
     mw.col.decks.select(deck['id'])
     mw.col.decks.get(deck)['mid'] = model['id']
     mw.col.decks.save(deck)
 
-    # ASSIGN DECK TO MODEL
+    # assign deck to model
     mw.col.models.setCurrent(model)
     mw.col.models.current()['did'] = deck['id']
     mw.col.models.save(model)
 
-    data = urllib.urlopen(URL)
+    # retrieve rss
+    data = urllib2.urlopen(URL)
     doc = BeautifulSoup.BeautifulStoneSoup(data, selfClosingTags=['link'])
 
-    for item in doc.findAll('entry'):
-        note = mw.col.newNote()
-        if not item.title is None:
-            note['Front']   = u''.join(unicode(i) for i in item.title.string)
-        if not item.summary is None:
-            note['Back']    = u''.join(unicode(i) for i in item.summary.string)
+    if not doc.find('entry') is None:
+        items = doc.findAll('entry')
+    elif not doc.find('item') is None:
+        items = doc.findAll('item')
+    else:
+        return
 
-        note['id'] = u''.join(unicode(i) for i in item.link['href'])
+    # iterate notes
+    for item in items:
+        note = mw.col.newNote()
+        note['Front'] = item.title.string
+        if not item.summary is None:
+            note['Back'] = item.summary.string
+        elif not item.description is None:
+            note['Back'] = item.description.string
+        if not item.link.string is None:
+            note['id'] = item.link.string
+        elif not item.link['href'] is None:
+            note['id'] = item.link['href']
+        else:
+            note['id'] = item.id.string
         note.tags = filter(None, tags)
         if note.dupeOrEmpty():
             continue
-        mw.col.addNote(card)
+        mw.col.addNote(note)
 
     mw.col.reset()
     mw.reset()
 
-# create a new menu item, "test"
-action = QAction("Build Card", mw)
-# set it to call testFunction when it's clicked
+# create a new menu item
+action = QAction("Feed to Anki", mw)
 mw.connect(action, SIGNAL("triggered()"), buildCard)
-# and add it to the tools menu
 mw.form.menuTools.addAction(action)
