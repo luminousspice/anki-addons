2589854481592946ebe06afac3ccd913ada6c4b1
Luminous Spice <luminous.spice@gmail.com>
2016-05-18 21:17:51 +0900

    Another Retreat: Release 0.1.1, fix an issue in continual lapses

--- Another_Retreat.py
+++ Another_Retreat.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Another Retreat:
 # an Anki addon sets the next interval at lapse to the last succeed interval.
+# Version: 0.1.1
 # GitHub: https://github.com/luminousspice/anki-addons/
 #
 # Copyright: 2016 Luminous Spice <luminous.spice@gmail.com>
@@ -11,8 +12,9 @@ from anki.sched import Scheduler
 
 def withdrawLapseIvl(self, card, conf):
     ''' Return the latest lastivl from revlog.'''
-    lastIvl = self.col.db.scalar("""
-select lastivl from revlog where cid = ? order by id desc""", card.id)
-    return max(conf['minInt'], lastIvl)
+    lastIvls = self.col.db.list("""
+select lastivl from revlog where cid = ? and type = 1 order by id desc""", card.id)
+    d = [x for x in lastIvls if x < card.ivl]
+    return max(conf['minInt'], d[0])
 
 Scheduler._nextLapseIvl = withdrawLapseIvl
