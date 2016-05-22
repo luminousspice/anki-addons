7b9043ada94e7ea595d221c975217b65b51984f1
Luminous Spice <luminous.spice@gmail.com>
2016-05-22 12:27:04 +0900

    Add 1minIvls

---
+++ 1minIvls.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# 1min Interval Counter: an Anki addon reports totals of 1min interval
+# in review log for each deck.
+# GitHub: https://github.com/luminousspice/anki-addons/
+#
+# Copyright: 2016 Luminous Spice <luminous.spice@gmail.com>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
+
+from aqt import mw
+from aqt.utils import showText
+from aqt.qt import *
+
+
+def onemin_ivls():
+    """Display totals of 1min ivl in revlog for each deck."""
+    list = []
+    r = "1min Interval count in review log for each deck\n\n"
+    dids = mw.col.decks.allIds()
+    for d in dids:
+        cids = mw.col.db.list("select id from cards where did = ?", d)
+        counter = 0
+        for x in cids:
+          counter += mw.col.db.scalar("select count() from revlog where lastivl = -60 and cid = ? order by id desc", x)
+        deck  = mw.col.decks.get(d)
+        print deck['name'] + u": "+ str(counter)
+        list.append([deck['name'],counter])
+    list = sorted(list,key=lambda x:x[0])
+    for l in list:
+      r += str(l[0])+": \t"+str(l[1])+"\n"
+    showText(r)
+
+
+action = QAction("1min Interval Report", mw)
+mw.connect(action, SIGNAL("triggered()"), onemin_ivls)
+mw.form.menuTools.addAction(action)
