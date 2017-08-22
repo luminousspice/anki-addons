db541c0b2ba2c784b1652c7665bc2572e096aa54
Luminous Spice <luminous.spice@gmail.com>
2017-08-22 15:18:39 +0900

    Fastbar: Add icons

--- fastbar/fastbar.py
+++ fastbar/fastbar.py
@@ -12,18 +12,21 @@
 # License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
 
 from aqt.qt import *
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from aqt.forms.browser import Ui_Dialog
 from aqt.browser import Browser
 from anki.sched import Scheduler
 from anki.utils import ids2str, intTime
 from anki.hooks import addHook, wrap
 
+from . import qtawesome as qta
+from . import six
+
 def addToolBar(self):
     tb = QToolBar("Toolbar")
     tb.setObjectName("Toolbar")
-    tb.setFloatable(False)
-    tb.setMovable(False)
+    tb.setIconSize(QtCore.QSize(16, 16))
+    tb.setToolButtonStyle(2)
 
     self.form.actionToggle_Sidebar.triggered.connect(self.toggleSidebar)
     self.form.actionToggle_Bury.triggered.connect(self.onBury)
@@ -31,19 +34,12 @@ def addToolBar(self):
     tb.addAction(self.form.actionToggle_Sidebar)
     tb.addSeparator()
     tb.addAction(self.form.actionAdd)
-    tb.addSeparator()
     tb.addAction(self.form.action_Info)
-    tb.addSeparator()
     tb.addAction(self.form.actionToggle_Mark)
-    tb.addSeparator()
     tb.addAction(self.form.actionToggle_Suspend)
-    tb.addSeparator()
     tb.addAction(self.form.actionToggle_Bury)
-    tb.addSeparator()
     tb.addAction(self.form.actionChange_Deck)
-    tb.addSeparator()
     tb.addAction(self.form.actionChangeModel)
-    tb.addSeparator()
     tb.addAction(self.form.actionAdd_Tags)
     tb.addAction(self.form.actionRemove_Tags)
     tb.addAction(self.form.actionClear_Unused_Tags)
@@ -87,11 +83,36 @@ def unburiedCards(self, ids):
         intTime(), self.col.usn())
 
 def setupUi(self, Dialog):
+    icon_sidebar = qta.icon('fa.exchange')
+    icon_add = qta.icon('fa.plus-square')
+    icon_info = qta.icon('fa.info-circle')
+    icon_mark = qta.icon('fa.star')
+    icon_suspend = qta.icon('fa.pause-circle')
+    icon_bury = qta.icon('fa.step-backward')
+    icon_deck = qta.icon('fa.inbox')
+    icon_note = qta.icon('fa.leanpub')
+    icon_tag = qta.icon('fa.tag')
+    icon_untag = qta.icon('fa.eraser')
+    icon_tag_unused = qta.icon('ei.remove-sign')
+    icon_delete = qta.icon('fa.trash-o')
+    self.actionAdd.setIcon(icon_add)
+    self.action_Info.setIcon(icon_info)
+    self.actionToggle_Mark.setIcon(icon_mark)
+    self.actionToggle_Suspend.setIcon(icon_suspend)
+    self.actionChange_Deck.setIcon(icon_deck)
+    self.actionChangeModel.setIcon(icon_note)
+    self.actionAdd_Tags.setIcon(icon_tag)
+    self.actionRemove_Tags.setIcon(icon_untag)
+    self.actionClear_Unused_Tags.setIcon(icon_tag_unused)
+    self.actionDelete.setIcon(icon_delete)
+
     self.actionToggle_Sidebar = QtWidgets.QAction(Dialog)
+    self.actionToggle_Sidebar.setIcon(icon_sidebar)
     self.actionToggle_Sidebar.setObjectName("toggleSidebar")
     self.actionToggle_Sidebar.setText(_("Toggle Sidebar"))
     self.actionToggle_Bury = QtWidgets.QAction(Dialog)
-    self.actionToggle_Bury.setObjectName("toggleBury")
+    self.actionToggle_Bury.setText(_("Toggle Bury"))
+    self.actionToggle_Bury.setIcon(icon_bury)
     self.actionToggle_Bury.setText(_("Toggle Bury"))
     self.menuJump.addSeparator()
     self.menuJump.addAction(self.actionToggle_Sidebar)
