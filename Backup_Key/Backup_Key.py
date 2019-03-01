2f0ad59f68be0a1e07554bff5fa69d38e9a2bb68
Luminous Spice <luminous.spice@gmail.com>
2019-03-01 22:18:25 +0900

    Backup_Key: Add notice, optimizing and checking db

--- Backup_Key/Backup_Key.py
+++ Backup_Key/Backup_Key.py
@@ -8,10 +8,41 @@
 
 from aqt import mw
 from aqt.qt import *
+from aqt.utils import showWarning, tooltip
+
+
+def backup_collection(self):
+    """ Backup the collection after optimizing and checking the db. """
+    if not mw.col:
+        return
+    label = _("Backing Up...")
+    mw.progress.start(label=label, immediate=True)
+    corrupt = False
+    try:
+        mw.maybeOptimize()
+    except:
+        corrupt = True
+    try:
+        mw.col.close()
+    except:
+        corrupt = True
+    if corrupt:
+        showWarning(_("Your collection file appears to be corrupt. \
+This can happen when the file is copied or moved while Anki is open, or \
+when the collection is stored on a network or cloud drive. If problems \
+persist after restarting your computer, please open an automatic backup \
+from the profile screen."))
+    else:
+        mw.backup()
+        mw.col.reopen()
+    mw.progress.finish()
+    if not corrupt:
+        tooltip(_("Backed up."))
+
 
 action = QAction(mw)
 action.setText(_("Save Backup"))
 action.setShortcut(_("Ctrl+S"))
-action.triggered.connect(mw.backup)
+action.triggered.connect(backup_collection)
 mw.form.menuCol.addSeparator()
 mw.form.menuCol.addAction(action)
