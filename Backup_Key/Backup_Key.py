2b99ad8a95732ef819c98e20cb4f97cddcd487e4
Luminous Spice <luminous.spice@gmail.com>
2019-03-01 22:20:36 +0900

    Backup Key: Release 0.1.0

--- Backup_Key/Backup_Key.py
+++ Backup_Key/Backup_Key.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# Copyright: 2017 Luminous Spice <luminous.spice@gmail.com>
-# License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
-#
 # Backup Key: an Anki add-on adds a shortcut to save a backup file
 # from the deck list on the main window.
 # GitHub: https://github.com/luminousspice/anki-addons/
+# Version: 0.1.0
+#
+# Copyright: 2019 Luminous Spice <luminous.spice@gmail.com>
+# License: GNU AGPL, version 3 or later; http://www.gnu.org/copyleft/agpl.html
 
 from aqt import mw
 from aqt.qt import *
